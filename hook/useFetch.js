faeefdfe87110c747403c04233ea43a9825781ef
konstantinos koliolios <kolioliosk@gmail.com>
2023-07-13 11:03:57 +0100

    fixing an error

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { RAPID_API_KEY } from '@env';
 
+
+const rapidapiKey = RAPID_API_KEY
 
 const useFetch = ({endpoint, query}) => {
   const [data, setData] = useState([]);
@@ -14,7 +17,7 @@ const useFetch = ({endpoint, query}) => {
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     params: { ...query },
     headers: {
-      'X-RapidAPI-Key': '5014948c37mshe6da381cea2625fp17a01bjsn28ffc9193cba',
+      'X-RapidAPI-Key': rapidapiKey,
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
     }
   };
